11ee84d546758afadac503b87b9c108d92dae977
Penjat <theSpencerSymington@Gmail.com>
2019-01-16 16:49:28 -0800

    Initial Commit

---
+++ HauntedHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HauntedHouse
+//
+//  Created by Spencer Symington on 2019-01-16.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
